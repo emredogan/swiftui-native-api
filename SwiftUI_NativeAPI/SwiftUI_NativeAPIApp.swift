ba8d9acbaf9900c0ca211086d1d423fa8be47b8d
Emre Dogan <emredogan@outlook.com>
2022-08-01 00:44:26 +0200

    Initial Commit

---
+++ SwiftUI_NativeAPI/SwiftUI_NativeAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_NativeAPIApp.swift
+//  SwiftUI_NativeAPI
+//
+//  Created by Emre Dogan on 01/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_NativeAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
